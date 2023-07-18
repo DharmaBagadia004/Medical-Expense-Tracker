0862c6abfff400533ba20c27d4adfad85f13d79a
DharmaBagadia004 <125524527+DharmaBagadia004@users.noreply.github.com>
2023-07-18 19:24:52 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
 import Home from './components/Home/Home';
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Login/>} />
+          <Route path='/home' element={<Home/>}  />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
